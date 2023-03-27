4880f7f62a99dcdde63cb2f0f95bfea7b7243997
Ramagiri10 <125990815+Ramagiri10@users.noreply.github.com>
2023-03-27 12:38:25 +0530

    again changed

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('** I HAVE CHAGED THE CODE SENTENCE BY FORKING THE FILE FEOM RAHAM :) **'); //write a response to the client
+  res.write('** This is my own creation code by sai :) **'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
